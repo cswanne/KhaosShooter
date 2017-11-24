0e18a952ed8071087ab4c86829c1ecbcfe850244
Mike Cockfield <cockfield@gmail.com>
2017-11-24 19:36:06 +0000

    Fix canister apprearing directly in front of asteriods
    Not perfect but sort of working

--- Khaos Shooter/Assets/Scripts/Consumables.cs
+++ Khaos Shooter/Assets/Scripts/Consumables.cs
@@ -13,21 +13,25 @@ public class Consumables : MonoBehaviour
     {
         if (clone == null) {
             Vector3 pos = new Vector3(0, 0, 0);
-//            GameObject asteriod = FindClosestAsteriod(pos);
-//            if (asteriod != null || asteriod.transform.position.x < pos.x + 50) return;
+            GameObject asteriod = FindClosestAsteriod(pos.y);
+            if (asteriod != null && asteriod.transform.position.x > pos.x && asteriod.transform.position.x < pos.x + 100) {
+                Assistant.canisterDestroyTime = Time.time;
+                return;
+            }
             Quaternion rot = Quaternion.Euler(new Vector3(90, 0, 0));
             clone = Instantiate(fuelCanister, pos, rot) as Transform;
         }
     }
 
-    private GameObject FindClosestAsteriod(Vector3 position)  {
+    private GameObject FindClosestAsteriod(float positionY)  {
         GameObject[] asteriods = GameObject.FindGameObjectsWithTag("Enemy");
         GameObject closest = null;
         float distance = Mathf.Infinity;
         foreach (GameObject obj in asteriods) {
-            Vector3 diff = (obj.transform.position - position);
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance) {
+            float diff = (obj.transform.position.y - positionY);
+            Vector3 compare = new Vector3(0, obj.transform.position.y, 0);
+            float curDistance = compare.sqrMagnitude;
+            if (diff >= -1 && diff <= 1 && curDistance < distance) { 
                 closest = obj;
                 distance = curDistance;
             }
