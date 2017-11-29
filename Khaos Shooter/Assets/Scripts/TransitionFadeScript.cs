ee409ce982786800645a6bd6a993992f22aac8c0
Swanne <swanne2@hotmail.com>
2017-11-29 14:05:53 +0000

    Transition Animation
    Had a little play with the animation for the transition scene

---
+++ Khaos Shooter/Assets/Scripts/TransitionFadeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionFadeScript : MonoBehaviour
+{
+
+    private CanvasGroup fade;
+    private bool fadeIn = true;
+
+    void Update()
+    {
+        if (fadeIn == true)
+        {
+            StartCoroutine(FadeTo(1.0f, 2.0f));
+        }
+        else
+        {
+            StartCoroutine(FadeTo(0.0f, 1.0f));
+        }
+    }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        fade = GetComponent<CanvasGroup>();
+        yield return new WaitForSeconds(1.0f);
+        float alpha = fade.alpha;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            fade.alpha = Mathf.Lerp(alpha, aValue, t);
+
+            yield return null;
+        }
+    }
+}
