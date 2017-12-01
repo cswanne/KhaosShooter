255e50d1730d88dacf373306f64a444789c1f76e
Swanne <swanne2@hotmail.com>
2017-12-01 13:58:11 +0000

    Scratch Scene
    Test scene for level design

---
+++ Khaos Shooter/Assets/Scripts/TempPlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed =  1f;
+    Rigidbody body;
+
+    public void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        float x = 0f, y = 0f;
+        if (Input.GetKey(KeyCode.RightArrow)) x = moveSpeed;
+        if (Input.GetKey(KeyCode.LeftArrow)) x = -moveSpeed;
+        if (Input.GetKey(KeyCode.UpArrow)) y = moveSpeed;
+        if (Input.GetKey(KeyCode.DownArrow)) y = -moveSpeed;
+        body.AddForce(moveSpeed, 0f, 0f, ForceMode.Force);
+    }
+
+}
