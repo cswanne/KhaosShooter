33aaa948b1d211ea95c450540f794e1a8a52e393
Mike Cockfield <cockfield@gmail.com>
2017-12-11 19:39:44 +0000

    Deplete fuel faster when boosting

--- Khaos Shooter/Assets/Scripts/Shield.cs
+++ Khaos Shooter/Assets/Scripts/Shield.cs
@@ -7,15 +7,10 @@ public class Shield : MonoBehaviour
 
     private float startTime = 0f;
     private float stayAliveCounter = 2;
-    private Light light1;
-    private Light light2;
 
     private void Start()
     {
         startTime = Time.time;
-        Light[] lights = transform.GetComponentsInChildren<Light>();
-        light1 = lights[0];
-        light2 = lights[1];
     }
 
 
@@ -23,12 +18,9 @@ public class Shield : MonoBehaviour
     {
         if (Time.time > startTime + stayAliveCounter) {
             Destroy(this.gameObject);
+            startTime = Time.time;
         }
-
-        light1.enabled = !(Time.deltaTime * 10 % 2 == 0);
-        light2.enabled = !(Time.deltaTime * 10 % 3 == 0);
     }
-
 
 
 }
