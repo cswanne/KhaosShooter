dd6ee86ae2647975f7baa6b27ab04f620f4cddb4
Swanne <swanne2@hotmail.com>
2018-04-23 21:52:21 +0100

    Canvas adjust and new parallax
    Optimised the canvas to make it appear correctly on all screen sizes. Also introduced new parallax method and started new graphics

---
+++ Khaos Shooter/Assets/Scripts/scrollingBackground.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingBackground : MonoBehaviour
+{
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private int leftIndex;
+    private int rightIndex;
+    private float lastCameraX;
+
+    public bool scrolling, parallax;
+    public float backgroundSize;
+    public float viewZone;
+    public float parallaxSpeed;
+
+
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraX = cameraTransform.position.x;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+
+    }
+
+    private void Update()
+    {
+        if (parallax)
+        {
+            float deltaX = cameraTransform.position.x - lastCameraX;
+            transform.position += Vector3.right * (deltaX * parallaxSpeed * Time.deltaTime);
+        }
+
+        lastCameraX = cameraTransform.position.x;
+
+        if (scrolling)
+        {
+            if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+                scrollLeft();
+            if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+                scrollRight();
+        }
+    }
+
+    private void scrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+            rightIndex = layers.Length - 1;
+    }
+
+    private void scrollRight()
+    {
+        int lastLeft = leftIndex;
+        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+            leftIndex = 0;
+    }
+
+}
